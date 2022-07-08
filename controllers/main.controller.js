90605ef8d9f750cde79ebaa6b29387a75012701c
omar fernando <construredomar@gmail.com>
2022-07-08 19:27:07 -0300

    segunda

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -8,11 +8,21 @@ module.exports ={
     // Step 3
     addCart: (req,res) => {
         // Find Product in DB
-        // let product = ???
+        let product = one(req.body.id)
         // Check product exist in cart
-        // Case 1: Exist and update quantity
-        // Case 2: Add cart and set quantity
-        return res.send("Add a new product")
+        if(req.session.cart.find(item => item.id == product.id)){
+            // Case 1: Exist and update quantity
+            req.session.cart = req.session.cart.map(item => {
+                if(item.id == product.id){
+                    item.quantity = item.quantity + 1
+                }
+                return item
+            })
+        }else{
+            // Case 2: Add cart and set quantity
+            req.session.cart.push({...product,quantity:1})
+        }
+        return res.redirect('/')
     },
     // Step 5
     updateCart: (req,res) => {
